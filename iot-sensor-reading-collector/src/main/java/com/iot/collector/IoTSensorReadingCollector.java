5e1fd5ee5209efa2b8bd9c5d073c11b9320899d0
tomorch <tom.zeroth@gmail.com>
2025-11-16 23:50:10 +0000

    Sensor Reading Collector consuming the sensor-reading stream and printing the readings. Logging not working.

--- iot-sensor-reading-collector/src/main/java/com/iot/collector/IoTSensorReadingCollector.java
+++ iot-sensor-reading-collector/src/main/java/com/iot/collector/IoTSensorReadingCollector.java
@@ -1,12 +1,37 @@
 package com.iot.collector;
 
-import org.apache.flink.datastream.api.ExecutionEnvironment;
+import com.iot.model.SensorReading;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.formats.json.JsonDeserializationSchema;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
 
 public class IoTSensorReadingCollector {
     static void main(String[] args) throws Exception {
-        final ExecutionEnvironment env = ExecutionEnvironment.getInstance();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        //JsonDeserializationSchema jsonDeserializationSchema = new JsonDeserializationSchema();
+        JsonDeserializationSchema<SensorReading> jsonDeserializationSchema =
+                new JsonDeserializationSchema<>(SensorReading.class);
+
+        KafkaSource<SensorReading> source = KafkaSource.<SensorReading>builder()
+                .setBootstrapServers("localhost:29092")
+                .setTopics("sensor-readings")
+                .setGroupId("sensor-reading-collector-group")
+                .setValueOnlyDeserializer(jsonDeserializationSchema)
+                .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Sensor Reading Source")
+            .process(new ProcessFunction<SensorReading, SensorReading>() {
+                @Override
+                public void processElement(SensorReading reading, Context ctx, Collector<SensorReading> out) {
+                    System.out.println("Received Sensor Reading: " + reading.toString());
+                    out.collect(reading); // forward if needed
+                }
+            });
+
+        env.execute("IoT Sensor Reading Collector");
     }
+
 }
