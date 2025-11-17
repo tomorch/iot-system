5c051382cfdf264ae9e5b7bc879e338ebacbb98a
tomorch <tom.zeroth@gmail.com>
2025-11-17 21:39:38 +0000

    Collector successfully publishing to Prometheus.

--- iot-sensor-reading-collector/src/main/java/com/iot/collector/IoTSensorReadingCollector.java
+++ iot-sensor-reading-collector/src/main/java/com/iot/collector/IoTSensorReadingCollector.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 public class IoTSensorReadingCollector {
     private static final Logger log = LoggerFactory.getLogger(IoTSensorReadingCollector.class);
 
-    static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         JsonDeserializationSchema<SensorReading> jsonDeserializationSchema =
