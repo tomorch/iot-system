b218ddb3dd636f269fdd88974a0882a66673ad8f
tomorch <tom.zeroth@gmail.com>
2025-11-21 14:28:12 +0000

    Cleanup.

--- iot-sensor-query-service/src/main/java/iot/sensor/query/IoTSensorQueryApplication.java
+++ iot-sensor-query-service/src/main/java/iot/sensor/query/IoTSensorQueryApplication.java
@@ -1,6 +1,5 @@
 package iot.sensor.query;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
