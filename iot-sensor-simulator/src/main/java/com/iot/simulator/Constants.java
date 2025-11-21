20995cd299c975edebdba91f3c6bcd23954cc987
tomorch <tom.zeroth@gmail.com>
2025-11-21 15:54:31 +0000

    Simulator cleanup.

--- iot-sensor-simulator/src/main/java/com/iot/simulator/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.iot.simulator;
-
-public class Constants {
-    public static final String SENSOR_TYPE_THERMOSTAT = "THERMOSTAT";
-    public static final String SENSOR_TYPE_HEART_RATE_MONITOR = "HEART_RATE_MONITOR";
-    public static final String SENSOR_TYPE_FUEL_GAUGE = "FUEL_GAUGE";
-}
